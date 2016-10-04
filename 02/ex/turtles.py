228eb00f22f79bd0f1f436c64c6a562b3f5f7089
Jamie Saxon <jsaxon@uchicago.edu>
2016-10-04 15:53:06 -0500

    more turtle power

---
+++ 02/ex/turtles.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python 
+
+import math
+import turtle 
+
+joe = turtle.Turtle()
+
+def square(joe):
+
+    # Use a for loop with fd() and lt() or rt(),
+    # to draw a square.
+    
+    pass
+
+square(joe)
+
+input("Hit return to end!")
+turtle.bye()
+
+
